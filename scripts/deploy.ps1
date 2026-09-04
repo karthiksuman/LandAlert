1030cdc320dead3e38a4e67a53ae386a7bcb5e61
Bharadwajsirasapalli <bharathsirasapalli63@gmail.com>
2026-09-04 19:35:46 +0530

    chore: configure repository for karthiksuman/LandAlert

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -2,11 +2,11 @@ $git = "C:\Users\ACER\AppData\Local\GitHubDesktop\app-3.6.4\resources\app\git\cm
 Push-Location dist
 if (Test-Path .git) { Remove-Item -Recurse -Force .git }
 & $git init
-& $git remote add origin https://github.com/bharathsirasapalli63-gif/SIH.git
+& $git remote add origin https://github.com/karthiksuman/LandAlert.git
 & $git checkout -B gh-pages
 & $git add -A
 & $git commit -m "Deploy LandAlert to GitHub Pages"
 & $git push -f origin gh-pages
 Remove-Item -Recurse -Force .git
 Pop-Location
-Write-Output "Successfully deployed real files to gh-pages!"
+Write-Output "Successfully deployed real files to gh-pages for karthiksuman/LandAlert!"
